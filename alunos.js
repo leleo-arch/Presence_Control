ce7657e53964d9fe809ba24c82bb4502e5a14381
leleo-arch <leo_xdplay@hotmail.com>
2024-10-16 21:58:10 -0300

    .

--- alunos.js
+++ alunos.js
@@ -1,58 +1,17 @@
-// Função para registrar presença e salvar no localStorage
 function registrarPresenca(element) {
     const alunoDiv = element.parentElement;
-    const alunoNome = alunoDiv.querySelector('.aluno-nome').innerText;
     const contador = alunoDiv.querySelector('.contador');
     let [presencas, ausencias] = contador.innerText.match(/\d+/g).map(Number);
 
     presencas++;
-
-    // Atualiza o contador visualmente
     contador.innerText = `Presenças: ${presencas} | Ausências: ${ausencias}`;
-
-    // Salva no localStorage
-    salvarNoLocalStorage(alunoNome, presencas, ausencias);
 }
 
-// Função para registrar ausência e salvar no localStorage
 function registrarAusencia(element) {
     const alunoDiv = element.parentElement;
-    const alunoNome = alunoDiv.querySelector('.aluno-nome').innerText;
     const contador = alunoDiv.querySelector('.contador');
     let [presencas, ausencias] = contador.innerText.match(/\d+/g).map(Number);
 
     ausencias++;
-
-    // Atualiza o contador visualmente
     contador.innerText = `Presenças: ${presencas} | Ausências: ${ausencias}`;
-
-    // Salva no localStorage
-    salvarNoLocalStorage(alunoNome, presencas, ausencias);
 }
-
-// Função para salvar os dados no localStorage
-function salvarNoLocalStorage(alunoNome, presencas, ausencias) {
-    const dadosAluno = {
-        presencas: presencas,
-        ausencias: ausencias
-    };
-    localStorage.setItem(alunoNome, JSON.stringify(dadosAluno));
-}
-
-// Função para carregar os dados do localStorage quando a página é carregada
-function carregarDadosAlunos() {
-    const alunos = document.querySelectorAll('.aluno');
-
-    alunos.forEach(aluno => {
-        const alunoNome = aluno.querySelector('.aluno-nome').innerText;
-        const contador = aluno.querySelector('.contador');
-        const dadosAluno = JSON.parse(localStorage.getItem(alunoNome));
-
-        if (dadosAluno) {
-            contador.innerText = `Presenças: ${dadosAluno.presencas} | Ausências: ${dadosAluno.ausencias}`;
-        }
-    });
-}
-
-// Chama a função de carregar dados ao abrir a página
-window.onload = carregarDadosAlunos;
