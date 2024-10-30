bfec5087f1e29234280fc00fd953534ce91e8653
leleo-arch <leo_xdplay@hotmail.com>
2024-10-29 21:52:30 -0300

    .

--- Containers/saida.js
+++ Containers/saida.js
@@ -42,22 +42,43 @@ function adicionarAlunoDOM(nome, tempo) {
         <span class="aluno-nome">${nome.toUpperCase()}</span>
         <div class="contador">Tempo: ${tempo}</div>
         <div class="cronometro-container">
+            <input type="text" class="input-tempo" placeholder="MM:SS" onblur="definirTempoManual(this)" />
             <button onclick="iniciarCronometro(this)">Iniciar/Reiniciar</button>
             <button onclick="pausarCronometro(this)">Pausar</button>
-            <button onclick="adicionarTempo(this, 5)">Adicionar 5s</button>
-                    <button class="excluir-aluno" onclick="excluirAluno(this)">Excluir</button>
-
+            <button class="excluir-aluno" onclick="excluirAluno(this)">Excluir</button>
         </div>
-        
     `;
     alunosLista.appendChild(alunoDiv);
+}
+
+// Função para definir o tempo manualmente
+function definirTempoManual(input) {
+    const alunoDiv = input.closest('.aluno');
+    const contador = alunoDiv.querySelector('.contador');
+    const nomeAluno = alunoDiv.querySelector('.aluno-nome').innerText;
+
+    const tempoManual = input.value.split(':');
+    if (tempoManual.length === 2) {
+        const minutos = parseInt(tempoManual[0]) || 0;
+        const segundos = parseInt(tempoManual[1]) || 0;
+        const totalSegundos = minutos * 60 + segundos;
+
+        if (intervalos[nomeAluno]) {
+            intervalos[nomeAluno].tempo = totalSegundos;
+        } else {
+            intervalos[nomeAluno] = { tempo: totalSegundos };
+        }
+
+        contador.innerText = `Tempo: ${formatarTempo(totalSegundos)}`;
+        salvarAlunos();
+    }
 }
 
 // Função para excluir um aluno
 function excluirAluno(button) {
     const alunoDiv = button.closest('.aluno');
-    alunoDiv.remove();  // Remove o aluno da lista na tela
-    salvarAlunos();  // Atualiza o localStorage após a exclusão
+    alunoDiv.remove();
+    salvarAlunos();
 }
 
 // Variáveis para o cronômetro
@@ -68,13 +89,17 @@ function iniciarCronometro(button) {
     const alunoDiv = button.closest('.aluno');
     const contador = alunoDiv.querySelector('.contador');
     const nomeAluno = alunoDiv.querySelector('.aluno-nome').innerText;
-    
+
     if (!intervalos[nomeAluno]) {
-        intervalos[nomeAluno] = { tempo: 0, intervalo: setInterval(() => {
+        intervalos[nomeAluno] = { tempo: 0 };
+    }
+
+    if (!intervalos[nomeAluno].intervalo) {
+        intervalos[nomeAluno].intervalo = setInterval(() => {
             intervalos[nomeAluno].tempo++;
             contador.innerText = `Tempo: ${formatarTempo(intervalos[nomeAluno].tempo)}`;
             salvarAlunos();
-        }, 1000) };
+        }, 1000);
     }
 }
 
@@ -82,20 +107,9 @@ function pausarCronometro(button) {
     const alunoDiv = button.closest('.aluno');
     const nomeAluno = alunoDiv.querySelector('.aluno-nome').innerText;
 
-    if (intervalos[nomeAluno]) {
+    if (intervalos[nomeAluno] && intervalos[nomeAluno].intervalo) {
         clearInterval(intervalos[nomeAluno].intervalo);
-        delete intervalos[nomeAluno];
-    }
-}
-
-function adicionarTempo(button, segundos) {
-    const alunoDiv = button.closest('.aluno');
-    const contador = alunoDiv.querySelector('.contador');
-    const nomeAluno = alunoDiv.querySelector('.aluno-nome').innerText;
-
-    if (intervalos[nomeAluno]) {
-        intervalos[nomeAluno].tempo += segundos;
-        contador.innerText = `Tempo: ${formatarTempo(intervalos[nomeAluno].tempo)}`;
+        delete intervalos[nomeAluno].intervalo;
     }
 }
 
